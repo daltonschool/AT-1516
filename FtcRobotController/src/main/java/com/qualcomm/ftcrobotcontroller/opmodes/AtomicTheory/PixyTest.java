40ee8f4fcc055dc87899a706a7d009bdf911eff2
Davis Haupt <dbh937@gmail.com>
2015-11-08 13:51:54 -0500

    began Pixy Cam test program

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AtomicTheory/PixyTest.java
@@ -0,0 +1,39 @@
+package com.qualcomm.ftcrobotcontroller.opmodes.AtomicTheory;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+/**
+ * Created by davis on 11/7/15.
+ */
+public class PixyTest extends LinearOpMode{
+
+  DeviceInterfaceModule dim;
+  public void runOpMode() throws InterruptedException{
+    dim = hardwareMap.deviceInterfaceModule.get("dim");
+    waitForStart();
+
+    while(opModeIsActive()) {
+      byte[] data = dim.getCopyOfReadBuffer(0);
+
+    }
+
+  }
+
+  /**
+   * Convert bytes to an word (Little Endian)
+   * @param b1
+   * @param b2
+   * @return little-endian word.
+   */
+  public short bytesToWord(byte b1, byte b2) {
+    ByteBuffer b = ByteBuffer.allocateDirect(2);
+    b.order(ByteOrder.LITTLE_ENDIAN);
+    b.put(b1);
+    b.put(b2);
+    b.flip();
+    return b.getShort();
+  }
+}
