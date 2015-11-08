31484c9173315b005b7c5c5eb5b3481857e957a5
Davis Haupt <dbh937@gmail.com>
2015-11-08 14:19:48 -0500

    basic debugging for Pixy I2C stream

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AtomicTheory/PixyTest.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AtomicTheory/PixyTest.java
@@ -15,9 +15,13 @@ public class PixyTest extends LinearOpMode{
     dim = hardwareMap.deviceInterfaceModule.get("dim");
     waitForStart();
 
+    short lastSync = 0x0000;
     while(opModeIsActive()) {
       byte[] data = dim.getCopyOfReadBuffer(0);
-
+      telemetry.addData("Bytes", "Value");
+      for (int i = 0; i < data.length; i += 2) {
+        telemetry.addData(i + ", " + i+1, bytesToWord(data[i], data[i+1]));
+      }
     }
 
   }
