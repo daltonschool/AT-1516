f5a9f93cd23271416d80d4d8cc423c33c3e1b5f2
Davis Haupt <dbh937@gmail.com>
2015-11-18 21:50:26 -0500

    documented anti-tip

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AtomicTheory/AntiTippingDemo.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AtomicTheory/AntiTippingDemo.java
@@ -14,8 +14,8 @@ public class AntiTippingDemo extends AlphaDrive{
   public void init() {
     super.init();
     app = ((FtcRobotControllerActivity)hardwareMap.appContext);
-    goal = app.ax;
-    k = .1;
+    goal = app.ax; // i'm not sure which axis we want to anti-tip on.
+    k = .01; // change this for more precision.
   }
   public void loop() {
     double p = scale_motor_power((goal - app.ax)*k);
