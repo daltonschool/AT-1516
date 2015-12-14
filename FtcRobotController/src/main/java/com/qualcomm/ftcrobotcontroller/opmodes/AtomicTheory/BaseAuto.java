02a8c659349d8cce8a0ff86aa3eefe7c208fa10e
Davis Haupt <dbh937@gmail.com>
2015-12-14 18:13:39 -0500

    added Autonomous methods

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AtomicTheory/BaseAuto.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AtomicTheory/BaseAuto.java
@@ -1,6 +1,7 @@
 package com.qualcomm.ftcrobotcontroller.opmodes.AtomicTheory;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 /**
@@ -9,6 +10,10 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public abstract class BaseAuto extends LinearOpMode{
   DcMotor encoderMotor1;
   DcMotor encoderMotor2;
+
+  ColorSensor colorSensor;
+
+  final int LINE_THRESHOLD = 20;
 
   /**
    * Drive the left side of the robot.
@@ -65,49 +70,102 @@ public abstract class BaseAuto extends LinearOpMode{
     moveRight(power * d);
   }
 
-  void driveTicks(double power, int ticks) {
-    int start = encoderMotor1.getCurrentPosition();
-    int start2 = encoderMotor2.getCurrentPosition();
-    double pl = power;
-    double pr = power;
-
-    double K = 1000.0;
-    double C = .02;
-    while (Math.abs(encoderMotor1.getCurrentPosition() - start) < ticks || Math.abs(encoderMotor2.getCurrentPosition() - start2) < ticks) {
-//      int e1 = encoderMotor1.getCurrentPosition();
-//      int e2 = encoderMotor2.getCurrentPosition();
-//      int diff = e1 - e2;
-//      if (diff > 0) {
-//        pl += .02;
-//        pr -= .02;
-//      } else if (diff < 0) {
-//        pl -= .02;
-//        pr += .02;
-//      } else {
-//        pl = power;
-//        pr = power;
-//      }
-//      moveLeft(scale(pl));
-//      moveRight(scale(pr));
-      drive(power);
-
-    }
-    stopMotors();
-  }
-
-  double scale(double d) {
-    if (d > 1) return 1;
-    if (d < -1) return -1;
-    else return d;
-  }
-
   void rotateTicks(double power, AtomicUtil.Direction dir, int ticks) {
     int start = encoderMotor1.getCurrentPosition();
     while(Math.abs(encoderMotor1.getCurrentPosition() - start) < ticks)
       rotate(power, dir);
     stopMotors();
   }
-  abstract AtomicUtil.Alliance getTeam();
 
-//  abstract AtomicUtil.Alliance getTeam();
+  void dumbTicks(double power, int ticks) {
+    int start = encoderMotor1.getCurrentPosition();
+    int start2 = encoderMotor2.getCurrentPosition();
+    while (Math.abs(encoderMotor1.getCurrentPosition() - start) < ticks
+            || Math.abs(encoderMotor2.getCurrentPosition() - start2) < ticks) {
+      drive(power);
+    }
+  }
+
+  void driveTicks(double power, int ticks) {
+    int startLeft;
+    int posLeft;
+
+    int startRight;
+    int posRight;
+
+    startLeft = posLeft = Math.abs(encoderMotor1.getCurrentPosition());
+    startRight = posRight = Math.abs(encoderMotor2.getCurrentPosition());
+
+    double pl = power;
+    double pr = power;
+
+    double K = .001;
+    double C = .02;
+    do {
+      int diff = posLeft - posRight;
+
+      pl += diff *  sign(diff) * K;
+      pr += diff * -sign(diff) * K;
+      pl = scale(pl);
+      pr = scale(pr);
+
+      moveLeft(pl);
+      moveRight(pr);
+
+      posLeft = Math.abs(encoderMotor1.getCurrentPosition());
+      posRight = Math.abs(encoderMotor2.getCurrentPosition());
+    } while (posLeft - startLeft < ticks || posRight - startRight < ticks);
+
+    stopMotors();
+  }
+
+  /**
+   * Ensure a number is within bounds for motor controllers.
+   * @param d number
+   * @return number, as long as it's within -1 and 1. Returns the bound otherwise.
+   */
+  double scale(double d) {
+    if (d > 1) return 1;
+    if (d < -1) return -1;
+    else return d;
+  }
+
+  /**
+   * Determine if a number is positive or negative
+   * @param d number
+   * @return 1 if number is positive, -1 if it's negative
+   */
+  double sign(double d) {
+    if (d >= 0) return 1.0;
+    else return -1.0;
+  }
+
+  /**
+   * Follow a white line, using a color sensor.
+   * This is an event-driven method. To continue to follow the line,
+   * keep calling this function. When you want to stop, call
+   * stopMotors() instead of this function.
+   *
+   * @param speed power to send to the motors.
+   */
+  void followLine(float speed) {
+    if (colorSensor.alpha() > LINE_THRESHOLD) {
+      moveLeft(speed);
+    } else {
+      moveRight(speed);
+    }
+  }
+
+  /**
+   * Follow a line for a given amount of time.
+   * @param millis time to follow the line for, in milliseconds.
+   */
+  void followLine(float speed, double millis) {
+    long t = System.currentTimeMillis();
+    while (System.currentTimeMillis() - t < millis)
+      followLine(speed);
+    stopMotors();
+  }
+
+  abstract AtomicUtil.Alliance getTeam();
 }
