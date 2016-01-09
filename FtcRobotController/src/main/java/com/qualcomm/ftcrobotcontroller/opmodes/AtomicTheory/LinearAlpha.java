204e9ae50bff30e8103ff3a9c71873aa4aeb6f0c
Davis Haupt <dbh937@gmail.com>
2016-01-09 14:27:21 -0500

    more fixes

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AtomicTheory/LinearAlpha.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AtomicTheory/LinearAlpha.java
@@ -12,13 +12,17 @@ public abstract class LinearAlpha extends BaseAuto {
   DcMotor left;
   DcMotor right;
   Servo aim;
-  Servo whack;
+  Servo dump;
+  Servo rightZip;
+  Servo leftZip;
   double aimCount;
+  double dumpCount;
+  double leftZipCount;
+  double rightZipCount;
   DcMotor pull;
 
 
-  Servo leftPersonDropper;
-  double leftPersonCount = 0;
+
 
   void moveLeft(double power) {
     left.setPower(power);
@@ -32,15 +36,20 @@ public abstract class LinearAlpha extends BaseAuto {
     right= encoderMotor2 = hardwareMap.dcMotor.get("right");
     pull = hardwareMap.dcMotor.get("pull");
     aim = hardwareMap.servo.get("aim");
-    leftPersonDropper = hardwareMap.servo.get("leftPersonServo");
-    whack = hardwareMap.servo.get("whack");
-    colorSensor1 = hardwareMap.colorSensor.get("colorSensor1");
-    colorSensor2 = hardwareMap.colorSensor.get("colorSensor2");
+    leftZip = hardwareMap.servo.get("leftZip");
+    rightZip = hardwareMap.servo.get("rightZip");
+    dump = hardwareMap.servo.get("dump");
 
-    leftPersonDropper.setPosition(leftPersonCount);
-    aimCount = .2;
+    aimCount = 0;
+    dumpCount = 1;
+    rightZipCount = .3;
+    leftZipCount = .7;
+
     aim.setPosition(aimCount);
-    whack.setPosition(.493);
+    dump.setPosition(dumpCount);
+    leftZip.setPosition(leftZipCount);
+    rightZip.setPosition(rightZipCount);
+
     left.setDirection(DcMotor.Direction.FORWARD);
     right.setDirection(DcMotor.Direction.REVERSE);
 
