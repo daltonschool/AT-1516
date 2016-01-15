68a65a030fbaecf62e28da84acff97e4b64a5848
higgs241 <nathanielostrer@gmail.com>
2016-01-15 18:18:28 -0500

    pushing

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AtomicTheory/BaseAutoNonLin.java
@@ -0,0 +1,276 @@
+package com.qualcomm.ftcrobotcontroller.opmodes.AtomicTheory;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by Nathaniel on 1/15/16
+ */
+
+public abstract class BaseAutoNonLin extends OpMode {
+    DcMotor encoderMotor1;
+    DcMotor encoderMotor2;
+
+    ColorSensor colorSensor1;
+    ColorSensor colorSensor2;
+
+    final int LINE_THRESHOLD = 20;
+
+    AdafruitIMU boschBNO055;
+    double startHeading = 0;
+    //The following arrays contain both the Euler angles reported by the IMU (indices = 0) AND the
+    // Tait-Bryan angles calculated from the 4 components of the quaternion vector (indices = 1)
+    volatile double[] rollAngle = new double[2], pitchAngle = new double[2], yawAngle = new double[2];
+
+    public void updateIMU() {
+        boschBNO055.getIMUGyroAngles(rollAngle, pitchAngle, yawAngle);
+    }
+
+    public double getHeading() {
+        updateIMU();
+        double corrected = yawAngle[0] - startHeading;
+        if (corrected > 0)
+            return corrected;
+        else
+            return corrected + 360;
+    }
+
+    public void resetHeading() {
+        getHeading();
+        startHeading = yawAngle[0];
+    }
+
+    abstract AtomicUtil.Alliance getTeam();
+
+    /**
+     * Drive the left side of the robot.
+     *
+     * @param power from -1.0 to 1.0
+     */
+    abstract void moveLeft(double power);
+
+    /**
+     * Drive the right side of the robot.
+     *
+     * @param power from -1.0 to 1.0
+     */
+    abstract void moveRight(double power);
+
+    /**
+     * Stop the motors.
+     */
+    void stopMotors() {
+        moveLeft(0);
+        moveRight(0);
+    }
+
+    /**
+     * Drive forward, moving both sides of the robot.
+     *
+     * @param power from -1.0 to 1.0
+     */
+    void drive(double power) {
+        moveLeft(power);
+        moveRight(power);
+    }
+
+    /**
+     * Rotate the robot.
+     *
+     * @param power double from 0.0.to 1.0
+     * @param dir Direction CLOCKWISE or COUNTERCLOCKWISE
+     */
+    void rotate(double power, AtomicUtil.Direction dir) {
+        int d;
+        switch (dir) {
+            case CLOCKWISE:
+                d = 1;
+                break;
+            case COUNTERCLOCKWISE:
+                d = -1;
+                break;
+            default:
+                d = 0;
+                break;
+        }
+        moveLeft(-power * d);
+        moveRight(power * d);
+    }
+
+    void rotateTicks(double power, AtomicUtil.Direction dir, int ticks) {
+        int start = encoderMotor1.getCurrentPosition();
+        while(Math.abs(encoderMotor1.getCurrentPosition() - start) < ticks)
+            rotate(power, dir);
+        stopMotors();
+    }
+
+    void rotateDegs(double power, AtomicUtil.Direction dir, double deg) {
+        resetHeading();
+        if (dir == AtomicUtil.Direction.CLOCKWISE)
+            while(getHeading() > (360 - deg))
+                rotate(power, dir);
+        else if (dir == AtomicUtil.Direction.COUNTERCLOCKWISE)
+            while (getHeading() < deg)
+                rotate(power, dir);
+    }
+
+    /**
+     * Drive forward until encoder tick threshold is met.
+     * @param power
+     * @param ticks Number of encoder ticks to travel
+     */
+    void dumbTicks(double power, int ticks) {
+        int start = encoderMotor1.getCurrentPosition();
+        int start2 = encoderMotor2.getCurrentPosition();
+        while (Math.abs(encoderMotor1.getCurrentPosition() - start) < ticks
+                || Math.abs(encoderMotor2.getCurrentPosition() - start2) < ticks) {
+            drive(power);
+        }
+    }
+
+    void driveTicks(double power, int ticks) {
+        resetHeading();
+        int startLeft = encoderMotor1.getCurrentPosition();
+        int startRight = encoderMotor2.getCurrentPosition();
+        double pl, pr;
+
+        pl = pr = power;
+
+        while (Math.abs(encoderMotor1.getCurrentPosition()- startLeft) < ticks ||
+                Math.abs(encoderMotor2.getCurrentPosition()- startRight) < ticks) {
+            double heading = getHeading();
+            if (heading > 180 && heading < 358) // if we're veering to the right,
+                pr += .1;
+            else if (heading < 180 && heading > 2) // if we're veering to the left,
+                pl += .1;
+            else // if we're going straight, back to normal power.
+                pl = pr = power;
+
+            pl = scale(pl);
+            pr = scale(pr);
+
+            moveLeft(pl);
+            moveRight(pr);
+        }
+        stopMotors();
+    }
+
+//  /**
+//   * Use a proportional control algorithm to drive a certain number of encoder
+//   * ticks forward.
+//   * @param power Base motor power. Motors will run around this speed.
+//   * @param ticks Number of encoder ticks to travel.
+//   */
+//  void driveTicks(double power, int ticks) {
+//    // Starting positions
+//    int startLeft;
+//    int startRight;
+//    // Current positions
+//    int posLeft;
+//    int posRight;
+//    // Motor power in the proportional control algorithm.
+//    double pl;
+//    double pr;
+//
+//    double K = .001; // proportionality constant
+//
+//    startLeft = Math.abs(encoderMotor1.getCurrentPosition());
+//    startRight = Math.abs(encoderMotor2.getCurrentPosition());
+//    pl = pr = power;
+//
+//    do {
+//      // update encoder positions
+//      posLeft = Math.abs(encoderMotor1.getCurrentPosition());
+//      posRight = Math.abs(encoderMotor2.getCurrentPosition());
+//      posLeft = Math.abs(posLeft - startLeft);
+//      posRight = Math.abs(posRight - startRight);
+//      telemetry.addData("left", posLeft);
+//      telemetry.addData("right", posRight);
+//
+//      int error = posLeft - posRight; // Discrepancy between the two encoders
+//      double correction = error * K; // Multiply that error by the constant to get the correction.
+//
+//      // correct the motor speeds
+//      pl += correction;
+//      pr -= correction;
+//
+//      // ensure we're still within the thresholds so we don't throw an error
+//      pl = scale(pl);
+//      pr = scale(pr);
+//
+//      // drive the motors
+//      moveLeft(pl);
+//      moveRight(pr);
+//    } while (posLeft - startLeft < ticks || posRight - startRight < ticks);
+//    // ^^ do all that while we still have encoder ticks left to run
+//
+//    stopMotors(); // stop motors before exiting
+//  }
+
+    /**
+     * Ensure a number is within bounds for motor controllers.
+     * @param d number
+     * @return number, as long as it's within -1 and 1. Returns the bound otherwise.
+     */
+    double scale(double d) {
+        if (d > 1) return 1.0;
+        if (d < -1) return -1.0;
+        else return d;
+    }
+
+    /**
+     * Determine if a number is positive or negative
+     * @param d number
+     * @return 1 if number is positive, -1 if it's negative
+     */
+    double sign(double d) {
+        if (d >= 0) return 1.0;
+        else return -1.0;
+    }
+
+    boolean seesLine(ColorSensor c) {
+        return c.alpha() > LINE_THRESHOLD;
+    }
+
+    /**
+     * Follow a white line, using a color sensor.
+     * This is an event-driven method. To continue to follow the line,
+     * keep calling this function. When you want to stop, call
+     * stopMotors() instead of this function.
+     *
+     * @param speed power to send to the motors.
+     */
+    void followLine(float speed) {
+        // If two color sensors are connected, use advanced line following.
+        if (colorSensor2 != null) {
+            if (seesLine(colorSensor1) && seesLine(colorSensor2)) // if both see the line
+                drive(speed); // drive straight.
+            else if (seesLine(colorSensor1)) // if only the left sensor sees the line
+                moveRight(speed); // turn to the right
+            else if (seesLine(colorSensor2)) // if only the right sensor sees the line
+                moveLeft(speed); // turn to the left
+            else // if neither sensor sees the line,
+                drive(speed); // drive straight.
+        }
+
+        // If only one sensor is connected, fallback to simple line following.
+        else {
+            if (seesLine(colorSensor1)) // if the sensor is on the line
+                moveLeft(speed); // move off the line
+            else // if the sensor is off the line
+                moveRight(speed); // move onto it
+        }
+    }
+
+    /**
+     * Follow a line for a given amount of time.
+     * @param millis time to follow the line for, in milliseconds.
+     */
+    void followLine(float speed, double millis) {
+        long t = System.currentTimeMillis();
+        while (System.currentTimeMillis() - t < millis)
+            followLine(speed);
+        stopMotors();
+    }
+}
---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AtomicTheory/NonLinAlpha.java
@@ -0,0 +1,240 @@
+package com.qualcomm.ftcrobotcontroller.opmodes.AtomicTheory;
+
+import android.util.Log;
+
+import com.qualcomm.robotcore.exception.RobotCoreException;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Hardware;
+
+/**
+ * Created by Nathaniel on 1/16/15.
+ */
+public abstract class NonLinAlpha extends BaseAutoNonLin {
+
+    double turnPower = .65;
+    double drivePower = 65;
+
+    DcMotor left;
+    DcMotor right;
+    Servo aim;
+    Servo dump;
+    Servo rightZip;
+    Servo leftZip;
+    double aimCount;
+    double dumpCount;
+    double leftZipCount;
+    double rightZipCount;
+    DcMotor pull;
+
+
+    void moveLeft(double power) {
+        left.setPower(power);
+    }
+
+    void moveRight(double power) {
+        right.setPower(power);
+    }
+
+    void stopMotors() {
+        while (left.getPower() != 0.0 || right.getPower() != 0) {
+            left.setPower(0);
+            right.setPower(0);
+        }
+    }
+
+    public void setup() {
+        left = encoderMotor1 = hardwareMap.dcMotor.get("left");
+        right = encoderMotor2 = hardwareMap.dcMotor.get("right");
+        pull = hardwareMap.dcMotor.get("pull");
+        aim = hardwareMap.servo.get("aim");
+        leftZip = hardwareMap.servo.get("leftZip");
+        rightZip = hardwareMap.servo.get("rightZip");
+        dump = hardwareMap.servo.get("dump");
+
+        aimCount = 0;
+        dumpCount = 1;
+        rightZipCount = .3;
+        leftZipCount = .7;
+
+        aim.setPosition(aimCount);
+        dump.setPosition(dumpCount);
+        leftZip.setPosition(leftZipCount);
+        rightZip.setPosition(rightZipCount);
+
+        left.setDirection(DcMotor.Direction.FORWARD);
+        right.setDirection(DcMotor.Direction.REVERSE);
+
+
+
+    /* gyro initialization */
+        curHeading = 0; //CHANGE BASED ON PROGRAM
+        hasStarted = false;
+        prevHeading = curHeading;
+
+        systemTime = System.nanoTime();
+        prevTime = systemTime;
+        try {
+            gyro = new AdafruitIMU(hardwareMap, "bno055"
+                    , (byte) (AdafruitIMU.BNO055_ADDRESS_A * 2)//By convention the FTC SDK always does 8-bit I2C bus
+                    , (byte) AdafruitIMU.OPERATION_MODE_IMU);
+        } catch (RobotCoreException e) {
+            Log.i("FtcRobotController", "Exception: " + e.getMessage());
+        }
+
+        systemTime = System.nanoTime();
+        gyro.startIMU();//Set up the IMU as needed for a continual stream of I2C reads.
+        telemetry.addData("FtcRobotController", "IMU Start method finished in: "
+                + (-(systemTime - (systemTime = System.nanoTime()))) + " ns.");
+
+        Thread headingThread = new Thread() {
+            public void run() {
+                while(true){
+                    updateHeadingThreaded();
+                }
+            }
+        };
+
+        headingThread.start();
+
+        //hopefully reduces lag during initial autonomous
+    }
+
+  /*gyro specific stuff*/
+
+    static AdafruitIMU gyro;
+
+    //The following arrays contain both the Euler angles reported by the IMU (indices = 0) AND the
+    // Tait-Bryan angles calculated from the 4 components of the quaternion vector (indices = 1)
+    static volatile double[] rollAngle = new double[2], pitchAngle = new double[2], yawAngle = new double[2], accs = new double[3];
+
+    boolean hasStarted;
+
+    static long systemTime;//Relevant values of System.nanoTime
+    static long elapsedTime;
+    static long prevTime;
+
+  /* heading is from -180 to 180 degrees */
+
+    static double curHeading;
+    static double prevHeading;
+    static double desiredHeading;
+
+    public void turnToHeading(double turnPower, double desiredHeading) {
+        updateHeading();
+        if (curHeading+180 > desiredHeading+180) {
+      /* might need a dead zone for turning... */
+            //Turn left until robot reaches the desiredHeading
+            while (curHeading+180 > desiredHeading+180) {
+                updateHeading();
+                moveLeft(turnPower);
+                moveRight(-turnPower);
+            }
+            stopMotors();
+        } else {
+            //Turn right until robot reaches the desiredHeading
+            while (curHeading+180 < desiredHeading+180) {
+                updateHeading();
+                moveLeft(turnPower);
+                moveRight(-turnPower);
+            }
+            stopMotors();
+        }
+        updateHeading();
+    }
+    public void rotateDegs(double turnPower, double degs) {
+        updateHeading();
+        double h = curHeading + degs * -sign(turnPower);
+        if (Math.abs(h) > 180)
+            h += -360*sign(h);
+        turnToHeading(Math.abs(turnPower), h);
+    }
+
+    /**
+     * Calculate one step of the proportional control for driving straight
+     * With the Adafruit IMU.
+     *
+     * driveTicksStraight allows us to drive straight using encoder ticks to determine when to stop,
+     * but we might want to stop driving when we reach other thresholds, such as a distance away
+     * from the wall measured by an Optical Distance Sensor.
+     *
+     * This function allows us to do that. Use it inside of a loop where the breakout condition is
+     * when you want to stop driving. Pass in the heading you want to drive to and the power
+     * you want to drive at, and right after the loop call stopMotors();
+     *
+     * @param power
+     * @param initHeading the heading to drive to
+     */
+    public void driveStraight(double power, double initHeading) {
+        updateHeading();
+
+        double error = curHeading - initHeading;
+        double error_const = .04;
+        double pl = scale(power - error*error_const);
+        double pr = scale(power + error*error_const);
+
+        moveLeft(pl);
+        moveRight(pr);
+    }
+
+    /**
+     * Drive forward until encoder tick threshold is met.
+     * @param power
+     * @param ticks Number of encoder ticks to travel
+     */
+    public void driveTicksStraight(double power, int ticks) {
+        int start = encoderMotor1.getCurrentPosition();
+        int start2 = encoderMotor2.getCurrentPosition();
+
+        updateHeading();
+        double initHeading = curHeading;
+        double error_const = .04;
+
+
+        while (Math.abs(encoderMotor1.getCurrentPosition() - start) < ticks
+                || Math.abs(encoderMotor2.getCurrentPosition() - start2) < ticks) {
+            updateHeading();
+
+            //gyro is too finicky to do integral stuff so just the basic derivative stuff
+            double pl = power;
+            double pr = power;
+
+
+            double error = curHeading - initHeading;
+
+            pl-=error * error_const;
+            pr+=error * error_const;
+
+            pl = scale(pl);
+            pr = scale(pr);
+
+            moveLeft(pl);
+            moveRight(pr);
+            telemetry.addData("m1:", Math.abs(encoderMotor1.getCurrentPosition() - start) - ticks);
+            telemetry.addData("m2:", Math.abs(encoderMotor2.getCurrentPosition() - start2) - ticks);
+        }
+
+        stopMotors();
+    }
+
+    public void updateHeading() {
+
+    }
+
+    public static void updateHeadingThreaded() {
+        elapsedTime = systemTime - prevTime;
+        prevTime = systemTime;
+        double elapsedSeconds = elapsedTime / 1000000000;
+        systemTime = System.nanoTime();
+
+        //Update gyro values
+        gyro.getIMUGyroAngles(rollAngle, pitchAngle, yawAngle);
+        prevHeading = curHeading;
+        curHeading = yawAngle[0];
+
+        //Display information on screen
+        //telemetry.addData("Headings(yaw): ",
+        //        String.format("Euler= %4.5f", yawAngle[0]));
+
+    }
+}
